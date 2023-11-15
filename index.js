4b970121c3f83d710d1fdeee00339e14c1f8a216
philipp <philipp.proff@gmx.de>
2023-11-15 18:45:32 +0100

    basic display of content in boxes

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let svg;
-let currentSession = 0;
-let boxWidth = 100;
-let boxHeight = 50;
-let radius = 50;
+const connectorHeight = 50;
+const connectorWidth = 100;
+const contentWidth = 100;
+const contentHeight = 50;
+const actorRadius = 50;
+// document.addEventListener('mousemove', event => {
+//     event.clientX
+//         //update connectors position according to scroll
+//         let connector = d3.select('#connector')
+//         connector.attr('x',window.scrollX+event.clientX-connectorWidth/2)
+//         connector.attr('y', window.scrollY+event.clientY-connectorHeight/2) 
+//         updateDataLinks()
+// })
+// document.addEventListener('scroll', event => {
+//     //update connectors position according to scroll
+//             //update connectors position according to scroll
+//             let connector = d3.select('#connector')
+//             connector.attr('x',window.scrollX+event.clientX-connectorWidth/2)
+//             connector.attr('y', window.scrollY+event.clientY-connectorHeight/2) 
+//             updateDataLinks()
+// })
 document.addEventListener('scroll', event => {
     //update connectors position according to scroll
     let connector = d3.select('#connector')
-    connector.attr('y', window.scrollY + window.innerHeight / 2 - boxHeight / 2) <
+    connector.attr('y', window.scrollY + window.innerHeight / 2 - connectorHeight / 2) 
     updateDataLinks()
-})
-document.addEventListener('mousemove', event => {
-
 })
 function init(course) {
     fetch(`./${course}/data.json`)
@@ -23,28 +37,54 @@ function init(course) {
         })
 }
 function generateCoordinatedData(json) {
+    let positionMapX = []
+    let positionMapY = []
     json.map((session, i) => {
         session.actors.map(actor => {
+            let [x,y] = generateCollisionlessCoordinates(positionMapX,positionMapY,i)
             actor.linePath = null
             actor.distance = null
-            actor.visible = false
-            actor.x = 100 + (window.innerWidth - 200) * Math.random()
-            actor.y = 100 + ((window.innerHeight - 200) * Math.random()) + window.innerHeight * i
+            actor.visible = true
+            actor.x = x
+            actor.y = y
         })
         session.content = session.content.map(content => {
+            let [x,y] = generateCollisionlessCoordinates(positionMapX,positionMapY,i)
             return {
                 text: content,
-                linePath:null,
-                distance:null,
-                visible:false,
-                x: 100 + (window.innerWidth - 200) * Math.random() ,
-                y: 100 + ((window.innerHeight - 200) * Math.random()) + window.innerHeight * i
+                linePath: null,
+                distance: null,
+                visible: true,
+                x: x,
+                y: y
             }
         })
     })
     console.log(json)
 }
-
+function generateCollisionlessCoordinates(positionMapX,positionMapY,i){
+    let fit = false;
+    let x = 0;
+    let y = 0;
+    while(fit == false){
+        x = 100 + (window.innerWidth - 200) * Math.random()
+        y = 100 + ((window.innerHeight  - 200) * Math.random()) + window.innerHeight * i
+        let found = false;
+        let n = 0;
+        while (found == false && n < positionMapX.length) {
+            if (Math.abs(positionMapX[n] - x) < 100 && Math.abs(positionMapY[n] - y) < 100) {
+                found = true;
+            }
+            n++;
+        }
+        if(found == false){
+            positionMapX.push(x)
+            positionMapY.push(y)
+            fit = true;
+        }
+    }
+    return [x,y]
+}
 function createStaticContent(json) {
     let width = window.innerWidth;
     let height = window.innerHeight * json.length;
@@ -61,13 +101,14 @@ function createStaticContent(json) {
     svg.append('g')
         .attr('id', 'sessions')
 
+
     connector = svg
         .append('rect')
         .attr('id', 'connector')
-        .attr('width', boxWidth)
-        .attr('height', boxHeight)
-        .attr('x', (d, i) => { return window.innerWidth / 2 - boxWidth / 2 })
-        .attr('y', (d, i) => { return window.innerHeight / 2 - boxHeight / 2 })
+        .attr('width', connectorWidth)
+        .attr('height', connectorHeight)
+        .attr('x', (d, i) => { return window.innerWidth / 2 - connectorWidth / 2 })
+        .attr('y', (d, i) => { return window.innerHeight / 2 - connectorHeight / 2 })
         .style('stroke', 'black')
         .style('fill', 'white')
         .append('text')
@@ -76,11 +117,18 @@ function createStaticContent(json) {
 }
 function createDataPoints(json) {
     let sessions = svg
-        .selectAll('#sessions g')
+        .select('#sessions')
+        .selectAll('g')
         .data(json)
         .enter()
         .append('g')
         .attr('class', "session")
+    
+    sessions.append('path')
+        .attr('stroke-dasharray', '5, 5')
+        .attr('stroke', 'rgba(12,12,234,0.2)')
+        .attr('fill', 'none')
+        .attr('d', (d, i) => { return "M0,"+window.innerHeight*i+"L"+window.innerWidth+","+window.innerHeight*i+""})
 
     let actors = sessions
         .selectAll('.actors')
@@ -89,12 +137,32 @@ function createDataPoints(json) {
         .append('g')
         .attr('class', "actors fixObjects")
         .attr('transform', (d, i) => { return `translate(${d.x},${d.y})` })
+        .on('click', (e, d) => {
+            if (d.role == "host") {
+                window.open(d.link, '_blank')
+            }
+        })
+    actors.append('circle')
+        .attr('r', actorRadius)
+        .attr('stroke', 'rgba(12,12,234,0.7)')
+        .attr('fill', d => { return 'url(#' + d.image + ')' })
+    actors.append('defs')
+        .append('pattern')
+        .attr('id', d => { return d.image })
+        .attr('height', "100%")
+        .attr('width', "100%")
+        .append('image')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('height', actorRadius * 2)
+        .attr('width', actorRadius * 2)
+        .attr('xlink:href', d => { return 'organigrams/' + d.image })
 
     actors.append('circle')
-        .attr('r', radius)
-        .attr('stroke', 'green')
+        .attr('r', actorRadius + 10)
+        .attr('stroke', 'none')
+        .attr('fill', 'none')
         .attr('id', (d, i) => { return "actor" + i })
-        .attr('fill', 'white')
         .attr('transform', 'rotate(180)')
 
     actors.append('text')
@@ -104,25 +172,24 @@ function createDataPoints(json) {
         .attr('alignment-baseline', "top")
         .text(d => d.name)
 
+
+
+
     let content = sessions
         .selectAll('.content')
         .data(d => d.content)
         .enter()
-        .append('g')
-        .attr('class', "content fixObjects")
+        .append('foreignObject')
         .attr('transform', (d, i) => { return `translate(${d.x},${d.y})` })
-
-    content.append('rect')
-        .attr('width', boxWidth)
-        .attr('height', boxHeight)
-        .attr('stroke', 'red')
-        .attr('fill', 'white')
-        .attr('z-index', 1)
-
-    content.append('text')
-        .attr('fill', 'black')
-        .attr('x', 5)
-        .text(d => d.text.split(' ')[0])
+        .attr('class', "content fixObjects")
+        .attr('width', connectorWidth)
+        .attr('height', connectorHeight)
+        .attr('style', 'text-align: justify;font-size: 2px')
+        .append('xhtml:body')
+        .append('xhtml:div')
+        .html(d => {
+            return marked.parse(d.text)
+        })
 }
 function updateDataLinks() {
     //calculate the curves for the connectors
@@ -133,14 +200,14 @@ function updateDataLinks() {
 function calculateBezierLine() {
     const line = d3.line().curve(d3.curveCatmullRom.alpha(0.5));
     let connector = d3.select('#connector')
-    let anchor1 = [parseFloat(connector.attr('x')) + boxWidth / 2, parseFloat(connector.attr('y')) + boxHeight / 2]
+    let anchor1 = [parseFloat(connector.attr('x')) + connectorWidth / 2, parseFloat(connector.attr('y')) + connectorHeight / 2]
     d3.selectAll('.fixObjects').each(function (d, i) {
         if (d.y > window.scrollY && d.y < window.scrollY + window.innerHeight) {
             d.visible = true
             let rect = this.classList.contains("content")
-            let anchor4 = [rect ? d.x + boxWidth / 2 : d.x,rect ?d.y + boxHeight / 2 : d.y]
-            let anchor2 = [0,anchor1[1]]
-            let anchor3 = [0,anchor4[1]]
+            let anchor4 = [rect ? d.x + connectorWidth / 2 : d.x, rect ? d.y + connectorHeight / 2 : d.y]
+            let anchor2 = [0, anchor1[1]]
+            let anchor3 = [0, anchor4[1]]
             //creating anchorpoints
             if (anchor4[0] > anchor1[0]) {
                 anchor2[0] = anchor1[0] + (anchor4[0] - anchor1[0]) / 4
@@ -150,22 +217,23 @@ function calculateBezierLine() {
                 anchor2[0] = anchor1[0] - (anchor1[0] - anchor4[0]) / 4
                 anchor3[0] = anchor4[0] + (anchor1[0] - anchor4[0]) / 4
             }
-            d.linePath = line([anchor1,anchor2,anchor3,anchor4])
-            d.distance = map_range(calculateDistanceY(anchor1,anchor4),0,window.innerHeight/2.5,1,0)
-        }else{
+            d.linePath = line([anchor1, anchor2, anchor3, anchor4])
+            d.distance = map_range(calculateDistanceY(anchor1, anchor4), 0, window.innerHeight / 2, 1, 0)
+        } else {
             d.visible = false
         }
     })
 }
-function calculateDistanceY(p1,p2) {
-    return Math.abs(p2[1]-p1[1])
- }
+function calculateDistanceY(p1, p2) {
+    return Math.abs(p2[1] - p1[1])
+}
 function drawLinks() {
     let curves = [];
     d3.selectAll('.fixObjects').each(function (d, i) {
         curves.push({
             line: d.linePath,
-            distance: d.distance
+            distance: d.distance,
+            visible: d.visible
         })
     })
     svg.selectAll('.dataLinks')
@@ -176,13 +244,15 @@ function drawLinks() {
                     .attr('class', 'dataLinks')
                     .attr('d', d => d.line)
                     .attr('stroke', d => `rgba(12,12,234,${d.distance})`)
+                    .attr('visibility', d => d.visible ? 'visible' : 'hidden')
                     .attr('fill', 'none')
                     .attr('stroke-width', 1)
             },
             function (update) {
                 return update
-                .attr('d', d => d.line)
-                .attr('stroke', d => `rgba(12,12,234,${d.distance})`)
+                    .attr('d', d => d.line)
+                    .attr('stroke', d => `rgba(12,12,234,${d.distance})`)
+                    .attr('visibility', d => d.visible ? 'visible' : 'hidden')
             }
         )
 }
