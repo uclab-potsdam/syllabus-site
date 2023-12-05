2ee3cf0549f40340058f02880efdd1cc62482dfe
philipp <philipp.proff@gmx.de>
2023-12-05 10:02:42 +0100

    padding for the items and same height for actors

--- js/data.js
+++ js/data.js
@@ -21,10 +21,10 @@ async function updateView(){
     sessions.map((session,sessionIndex) => {
         session.items.map((item,itemIndex) => {
             item.bounding = item.domObject.getBoundingClientRect()
-            item.height = item.bounding.height
-            if (itemIndex === 0) item.margin = 0
+            item.height = item.bounding.height + window.innerHeight * 0.1
+            if (itemIndex === 0 || item.type == "actor") item.margin = 0
             else item.margin = session.items[itemIndex - 1].margin + session.items[itemIndex - 1].height            
-            session.height += item.height 
+            session.height += item.height + window.innerHeight * 0.1
         })
         session.margin = sessionIndex == 0 ? 0 : sessions[sessionIndex - 1].margin + sessions[sessionIndex - 1].height
         session.items.map((item,itemIndex) => {
