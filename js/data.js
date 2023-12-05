df31b4e72dfd54c75d9ee7ff0549faffdb49c4fe
Marian Dörk <doerk@fh-potsdam.de>
2023-12-05 08:32:52 +0100

    actor label adjustments

--- js/data.js
+++ js/data.js
@@ -88,11 +88,12 @@ function createDataRepresentation(item) {
         svg.setAttribute("width", "100%");
         svg.setAttribute("height", "100%");
         svg.setAttribute('viewBox', '0 0 200 200')
-        svg.setAttribute('style', 'margin-top: -15px')
+        svg.setAttribute('style', 'margin-top: -60px')
 
         // Create a path element for the curve
         let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", "M30 15 Q 95 65 180 10");
+        //path.setAttribute("d", "M30 15 Q 95 65 180 10");
+        path.setAttribute("d", "M0 40 Q 100 150 200 40");        
         path.setAttribute("fill", "none");
         path.setAttribute("stroke", "none");
         path.setAttribute("id", "curve")
